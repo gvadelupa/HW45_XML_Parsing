8146592c3c6bd2695791c914c246ab77be3043c2
I G <gvadelupairina@yahoo.com>
2016-05-12 01:26:46 -0700

    HW 45

---
+++ src/main/java/core/HW45_XML_Parsing/App.java
@@ -0,0 +1,13 @@
+package core.HW45_XML_Parsing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
